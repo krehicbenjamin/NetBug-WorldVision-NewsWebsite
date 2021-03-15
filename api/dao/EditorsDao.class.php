2ca27896dce3eeb76e6356de04cdda24020b2339
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-15 13:26:55 +0100

    adding editor search and add function

--- api/dao/EditorsDao.class.php
+++ api/dao/EditorsDao.class.php
@@ -2,7 +2,28 @@
 
 require_once dirname(__FILE__)."/BaseDao.class.php";
 
-class EditorsDao{
+class EditorsDao extends BaseDao{
+
+    public function get_editor_by_email($editor_email){
+        return $this->query_unique("SELECT * FROM editors WHERE editor_email = :editor_email", ["editor_email" => $editor_email]);
+     }
+ 
+     public function get_editor_by_id($editor_id){
+         return $this->query_unique("SELECT * FROM editors WHERE editor_id = :editor_id", ["editor_id" => $editor_id]);
+     }
+ 
+     public function add_editor($editor){
+     
+         $sql = "INSERT INTO editors (editor_name, editor_pw, editor_email,) VALUES (:editor_name, :editor_pw, :editor_email)";
+         $stmt= $this->connection->prepare($sql);  //$pdo is $this->connection
+         $stmt->execute($editor);
+ 
+         // Getting the last added users id and returning it 
+ 
+         $editor['id'] = $this->connection->lastInsertId();
+         return $editor;
+ 
+     }
     
 }
 
