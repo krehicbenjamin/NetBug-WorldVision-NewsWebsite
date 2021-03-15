2fc349635bbfcc38a36c914f00308bdc921471f6
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-15 16:16:22 +0100

    Adding add_arrticle method

--- api/dao/ArticlesDao.class.php
+++ api/dao/ArticlesDao.class.php
@@ -3,6 +3,31 @@
 require_once dirname(__FILE__)."/BaseDao.class.php";
 
 class ArticleDao{
+
+    public function add_article($article){
+
+        $sql = "INSERT INTO articles (title, body, picture64, editor, added_at, category) VALUES (:title, :body, :picture64, :editor, :added_at, :category)";
+        $stmt= $this->connection->prepare($sql);  //$pdo is $this->connection
+        $stmt->execute($article);
+
+        // Getting the last added artiles id and returning it 
+
+        $article['article_id'] = $this->connection->lastInsertId();
+        return $article;
+
+
+    }
+
+
+    public function edit_article($article_id, $article){
+
+    }
+
+
+    public function delete_article($article_id){
+
+    }
+
     
 }
 
