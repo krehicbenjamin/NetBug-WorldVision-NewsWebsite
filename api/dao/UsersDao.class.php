e606acb6145c0dad3662e40d2e2184a10447187a
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-15 13:25:36 +0100

    creating an add user function

--- api/dao/UsersDao.class.php
+++ api/dao/UsersDao.class.php
@@ -9,7 +9,7 @@ class UsersDao extends BaseDao{
     }
 
     public function get_user_by_id($user_id){
-        return $this->query_unique("SELECT * FROM users WHERE user_id = :user_id", ["user_id" => $id]);
+        return $this->query_unique("SELECT * FROM users WHERE user_id = :user_id", ["user_id" => $user_id]);
     }
 
     public function add_user($user){
