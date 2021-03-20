29c07177a5c906b6fcc71ffc1f67b426beacb3d4
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-20 16:06:49 +0100

    Project setup fix

--- api/index.php
+++ api/index.php
@@ -3,12 +3,8 @@
    require_once dirname(__FILE__)."/dao/BaseDao.class.php"; 
 
    require dirname(__FILE__)."/../vendor/autoload.php";
-   require dirname(__FILE__)."/dao/AccountsDao.class.php";
+  
 
-
-   Flight::route('GET /accouts',function(){
-     
-   });
    
 
 
