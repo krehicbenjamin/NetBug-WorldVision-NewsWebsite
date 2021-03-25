67cafaaffbe9f857c437fb9aeccc86af253f717d
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-25 18:03:26 +0100

    adding routes for articles

--- api/index.php
+++ api/index.php
@@ -9,17 +9,34 @@
    Flight::route('/',function(){
      echo("Hello route!");
    });
+
+   Flight::register("articleDao", "ArticlesDao");
+
    Flight::route('GET /articles',function(){
-      $dao = new ArticlesDao();
-      $articles = $dao->get_all(0,10);
+      $articles = Flight::articleDao()->get_all(0,10);
       Flight::json($articles);
   });
-  
+
   Flight::route('GET /articles/@id',function($id){
     $dao = new ArticlesDao();
     $articles = $dao->get_by_id($id);
     Flight::json($articles);
 });
+
+  Flight::route('POST /articles',function(){
+    $request = Flight::request();
+    $data = $request->data->getData();
+    $article = Flight::articleDao()->add($data); 
+    Flight::json ($article); 
+  });
+
+  Flight::route('PUT /articles/@id',function($id){
+    $request = Flight::request();
+    $data = $request->data->getData();
+    $article = Flight::articleDao()->update($id, $data);
+    $article = Flight::articleDao()->get_by_id($id);  
+    Flight::json ($article); 
+  });
 
    Flight::start();
 ?>
