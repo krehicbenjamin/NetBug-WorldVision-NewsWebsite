f01f607ec2b8adda3eb5259b2d15b984357ea585
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-27 09:15:36 +0100

    adding routes for users

---
+++ api/routes/users.php
@@ -0,0 +1,29 @@
+<?php   
+    Flight::route('GET /users', function(){
+        $offset = Flight::query_param('offset', 0);
+        $limit = Flight::query_param('limit', 0);
+        $users = Flight::userDao()->get_all($offset, $limit);
+        Flight::json($users);
+    });
+    
+    Flight::route('GET /users/@id', function($id){
+        $users = Flight::userDao()->get_by_id($id);
+        Flight::json($users);
+    });
+    
+    Flight::route('POST /users', function(){
+        $request = Flight::request();
+        $data = $request->data->getData();
+        $user = Flight::userDao()->add($data); 
+        Flight::json ($user); 
+    });
+    
+    Flight::route('PUT /users/@id', function($id){
+        $request = Flight::request();
+        $data = $request->data->getData();
+        $user = Flight::userDao()->update($id, $data);
+        $user = Flight::userDao()->get_by_id($id);
+        Flight::json($user);
+    });
+
+?>
