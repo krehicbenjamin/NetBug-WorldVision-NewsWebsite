4475c2313273be3387f9184a2fcd2c264b278c33
krehicbenjamin <krehicbenjamin@gmail.com>
2021-04-10 15:45:40 +0200

    Adding order capability

--- api/routes/articles.php
+++ api/routes/articles.php
@@ -1,37 +1,33 @@
 <?php 
     Flight::route('GET /articles',function(){
         $offset = Flight::query_param('offset', 0);
-        $limit = Flight::query_param('limit', 10);
+         $limit = Flight::query_param('limit', 10);
         $search = Flight::query_param('search');
-        if($search){
-            Flight::json(Flight::articleDao()->get_articles($search, $offset, $limit));
-        } else {
-            Flight::json(Flight::articleDao()->get_all($offset, $limit));
-        }
+        $order = Flight::query_param('order');
+        Flight::json(Flight::articleService()->get_articles($search, $offset, $limit, $order)); 
     });
 
     Flight::route('GET /articles/@id',function($id){
-        $articles = Flight::articleDao()->get_by_id($id);
+        $articles = Flight::articleService()->get_by_id($id);
         Flight::json($articles);
     });
 
     Flight::route('POST /articles',function(){
         $request = Flight::request();
         $data = $request->data->getData();
-        $article = Flight::articleDao()->add($data); 
+        $article = Flight::articleService()->add($data); 
         Flight::json ($article); 
     });
 
     Flight::route('PUT /articles/@id',function($id){
         $request = Flight::request();
         $data = $request->data->getData();
-        $article = Flight::articleDao()->update($id, $data);
-        $article = Flight::articleDao()->get_by_id($id);  
+        $article = Flight::articleService()->update($id, $data);
         Flight::json ($article); 
     });
 
     Flight::route('DELETE /articles/@id',function($id){
-        $articles = Flight::articleDao()->delete_article($id);
+        $articles = Flight::articleService()->delete_article($id);
         Flight::json($articles);
     });
 
