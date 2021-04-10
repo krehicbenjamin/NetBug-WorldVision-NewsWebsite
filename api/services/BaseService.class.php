bd29db9cc0c4cf97f206d7dae8112fc8006ed3fa
krehicbenjamin <krehicbenjamin@gmail.com>
2021-04-10 15:47:11 +0200

    Adding base service universal functions

---
+++ api/services/BaseService.class.php
@@ -0,0 +1,21 @@
+<?php 
+
+class BaseService{
+
+    protected $dao;
+
+    public function get_by_id($id){
+        return $this->dao->get_by_id($id);
+    }
+    public function add($data){
+        return $this->dao->add($data);
+    }
+    public function update($id, $data){
+        $this->dao->update($id, $data);
+        return $this->dao->get_by_id($id);  
+    }
+   
+
+}
+
+?>
