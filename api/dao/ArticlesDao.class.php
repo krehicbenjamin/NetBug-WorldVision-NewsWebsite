7bfceb04d94ff58caa0391da1c827a936287a8d4
krehicbenjamin <krehicbenjamin@gmail.com>
2021-04-10 15:43:40 +0200

    Add search and order type capability

--- api/dao/ArticlesDao.class.php
+++ api/dao/ArticlesDao.class.php
@@ -8,9 +8,15 @@ class ArticlesDao extends BaseDao{
         parent::__construct("articles");
     }
 
-    public function get_articles($search, $offset, $limit){
-        return $this->query("SELECT * FROM articles WHERE LOWER(title) LIKE CONCAT('%', :title, '%') LIMIT ${limit} OFFSET ${offset}", ["title" => strtolower($search)]);
-    }
+    public function get_articles($search, $offset, $limit, $order = "-id"){
+        list($order_column, $order_direction) = self::parse_order($order);
+        return $this->query("SELECT *
+                             FROM articles
+                             WHERE LOWER(title) LIKE CONCAT('%', :title, '%')
+                             ORDER BY ${order_column} ${order_direction}
+                             LIMIT ${limit} OFFSET ${offset}",
+                             ["title" => strtolower($search)]);
+      }
 
     public function delete_article($id){
         return $this->query_unique("DELETE FROM articles WHERE id = :id", ["id" => $id]);
