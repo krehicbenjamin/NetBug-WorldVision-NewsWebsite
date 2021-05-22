528233aa38dd702dc22de1a55ac0a369067d06b8
krehicbenjamin <krehicbenjamin@gmail.com>
2021-05-22 16:06:30 +0200

    Adding key features and utility functions

--- api/index.php
+++ api/index.php
@@ -7,16 +7,26 @@
 
    /* require services */
    require_once dirname(__FILE__)."/services/ArticleService.class.php";   
-   require_once dirname(__FILE__)."./services/UserService.class.php";
+   require_once dirname(__FILE__)."/services/UserService.class.php";
 
-   /* Utility function */ 
-
+   /* Utility function */  
   Flight::map('query_param', function($name, $default_value = 0){
     $request = Flight::request();
     $query_param = @$request->query->getData()[$name];
     $query_param = $query_param ? $query_param : $default_value; 
     return $query_param;  
   });
+  /* utility function for getting header parameters */
+Flight::map('header', function($name){
+  $headers = getallheaders();
+  return @$headers[$name];
+});
+
+/* utility function for generating JWT token */
+Flight::map('jwt', function($user){
+  $jwt = \Firebase\JWT\JWT::encode(["exp" => (time() + Config::JWT_TOKEN_TIME), "id" => $user["id"], "aid" => $user["account_id"], "r" => $user["role"]], Config::JWT_SECRET);
+  return ["token" => $jwt];
+});
 
   /* require routes */
   require_once dirname(__FILE__)."/routes/articles.php";
@@ -25,5 +35,7 @@
   /* require BLL */
   Flight::register("articleService", "ArticleService");
   Flight::register("userService", "UserService");
+
+  
   Flight::start();
 ?>
