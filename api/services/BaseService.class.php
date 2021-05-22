f477c5cfebff778ab11bd3de6b2ac85e888148e3
krehicbenjamin <krehicbenjamin@gmail.com>
2021-05-22 16:03:06 +0200

    Creating a base service

--- api/services/BaseService.class.php
+++ api/services/BaseService.class.php
@@ -14,6 +14,9 @@ class BaseService{
         $this->dao->update($id, $data);
         return $this->dao->get_by_id($id);  
     }
+    public function get_all(){
+        return $this->dao->get_all();
+    }
    
 
 }
