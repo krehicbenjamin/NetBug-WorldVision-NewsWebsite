dc76adda38eb9ca0c6ab154161207a5106966bc3
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-16 11:48:18 +0200

    adding admin/user routes

--- api/routes/articles.php
+++ api/routes/articles.php
@@ -1,5 +1,5 @@
 <?php 
-    Flight::route('GET /articles',function(){
+    Flight::route('GET /user/articles',function(){
         $offset = Flight::query_param('offset', 0);
         $limit = Flight::query_param('limit', 10);
         $search = Flight::query_param('search');
@@ -7,26 +7,30 @@
         Flight::json(Flight::articleService()->get_articles($search, $offset, $limit, $order)); 
     });
 
-    Flight::route('GET /articles/@id',function($id){
+    Flight::route('GET /user/articles/@id',function($id){
         $articles = Flight::articleService()->get_by_id($id);
-        Flight::json($articles);
+        if ($article['id'] != Flight::get('user')['aid']){
+            throw new Exception("Invalid campaign", 403);
+          }else{
+            Flight::json($articles);
+          }
     });
 
-    Flight::route('POST /articles',function(){
+    Flight::route('POST /admin/articles',function(){
         $request = Flight::request();
         $data = $request->data->getData();
         $article = Flight::articleService()->add($data); 
         Flight::json ($article); 
     });
 
-    Flight::route('PUT /articles/@id',function($id){
+    Flight::route('PUT /admin/articles/@id',function($id){
         $request = Flight::request();
         $data = $request->data->getData();
         $article = Flight::articleService()->update($id, $data);
         Flight::json ($article); 
     });
 
-    Flight::route('DELETE /articles/@id',function($id){
+    Flight::route('DELETE /admin/articles/@id',function($id){
         $articles = Flight::articleService()->delete_article($id);
         Flight::json($articles);
     });
