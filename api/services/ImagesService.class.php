66ef1ff2f800ef8bffc9833f148759efde695d54
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-16 11:57:41 +0200

    adding general layout for service images

---
+++ api/services/ImagesService.class.php
@@ -0,0 +1,12 @@
+<?php 
+    require_once dirname(__FILE__)."/BaseService.class.php";
+    require_once dirname(__FILE__)."/../dao/ImagesDao.class.php";
+
+    class ImagesService extends BaseService{
+       
+        public function __construct(){
+            $this->dao = new ImagesDao(); 
+        }
+
+    }
+?>
