d65bd49b181c1cde8dbc41b74071f5a96ac63251
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-16 11:48:04 +0200

    updating middleware

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -3,9 +3,6 @@
 Flight::route('/user/*', function(){
   try {
     $user = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
-    if (Flight::request()->method != "GET" && $user["r"] == "USER_READ_ONLY"){
-      throw new Exception("Read only user can't change anything.", 403);
-    }
     Flight::set('user', $user);
     return TRUE;
   } catch (\Exception $e) {
