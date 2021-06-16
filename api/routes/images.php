b6e34eafb7d7a198607fef90cab4a12b216a2034
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-16 12:26:47 +0200

    Adding image routes

---
+++ api/routes/images.php
@@ -0,0 +1,22 @@
+<?php 
+
+    Flight::route('GET /user/images/@id',function($id){
+        $images = Flight::imagesService()->get_by_id($id);  //images dao function for searching images by article ID is needed
+        Flight::json($images);
+    });
+
+    Flight::route('POST /admin/images',function(){
+        $request = Flight::request();
+        $data = $request->data->getData();
+        $category = Flight::imagesService()->add($data); 
+        Flight::json ($category); 
+    });
+
+    Flight::route('PUT /admin/images/@id',function($id){
+        $request = Flight::request();
+        $data = $request->data->getData();
+        $images = Flight::imagesService()->update($id, $data);
+        Flight::json ($images); 
+    });
+
+?>
