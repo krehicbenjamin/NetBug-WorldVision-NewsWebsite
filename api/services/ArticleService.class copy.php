33df366f66aab7467fcd80f9d28c82f3cbc603ca
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-16 12:08:50 +0200

    adding aditional functionality to articleService

--- api/services/ArticleService.class copy.php
+++ api/services/ArticleService.class copy.php
@@ -19,7 +19,21 @@
         public function delete_article($id){
             $this->dao->delete_article($id);
        }
+       public function add_article($article){
+          $article_add['title'] = $article['title'];
+          $article_add['body'] = $article['body'];
+          $article_add['picture64'] = $article['picture64'];
+          $article_add['added_at'] = date(Config::DATE_FORMAT);
+          return parent::add($article_add);
+        
+      }
+    
+      public function update_article($id, $article){
+        $article = $this->dao->get_by_id($id);
+        return $this->update($id, $article);
+      }
+    }
    
 
-    }
+    
 ?>
