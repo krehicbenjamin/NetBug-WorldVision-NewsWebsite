0245bc90269012cb884c1d743a8203e1da07605c
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-16 12:21:44 +0200

    adding categories routes

---
+++ api/routes/categories.php
@@ -0,0 +1,29 @@
+<?php 
+    Flight::route('GET /user/categories',function(){
+        $offset = Flight::query_param('offset', 0);
+        $limit = Flight::query_param('limit', 10);
+        $search = Flight::query_param('search');
+        $order = Flight::query_param('order');
+        Flight::json(Flight::categoriesService()->get_categories($search, $offset, $limit, $order)); 
+    });
+
+    Flight::route('GET /user/categories/@id',function($id){
+        $category = Flight::categoriesService()->get_by_id($id);
+        Flight::json($category);
+    });
+
+    Flight::route('POST /admin/categories',function(){
+        $request = Flight::request();
+        $data = $request->data->getData();
+        $category = Flight::categoriesService()->add($data); 
+        Flight::json ($category); 
+    });
+
+    Flight::route('PUT /admin/categories/@id',function($id){
+        $request = Flight::request();
+        $data = $request->data->getData();
+        $category = Flight::categoriesService()->update($id, $data);
+        Flight::json ($category); 
+    });
+
+?>
