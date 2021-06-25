c38b30f843004dfe59bea2ee7a6506f92fa00605
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-26 00:51:21 +0200

    adding comment service

---
+++ api/services/CommentsService.class.php
@@ -0,0 +1,16 @@
+<?php 
+    require_once dirname(__FILE__)."/BaseService.class.php";
+    require_once dirname(__FILE__)."/../dao/CommentsDao.class.php";
+
+    class CommentsService extends BaseService{
+       
+        public function __construct(){
+            $this->dao = new CommentsDao(); 
+        }
+
+        public function get_article_comments($id){
+            return($this->dao->get_art_images($id));
+        }
+        
+    }
+?>
