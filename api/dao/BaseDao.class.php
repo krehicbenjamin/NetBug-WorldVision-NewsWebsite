bc9a4e1b0d2bf15e1ee3454eaf2a58cd0ed3dee3
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-27 14:38:15 +0200

    fixing db connection

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -41,7 +41,7 @@ class BaseDao{
             $this->table = $table;
 
             try{
-                $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DC_USERNAME, Config::DB_PASSWORD);
+                $this->connection = new PDO("mysql:host=".Config::DB_HOST().";port=".Config::DB_PORT().";dbname=".Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
                 // set the PDO error mode to exception
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
