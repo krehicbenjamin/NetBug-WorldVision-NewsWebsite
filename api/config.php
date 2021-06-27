ee343c7dc33cd602ba6dafabf7b1febfb8d994c8
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-27 14:38:00 +0200

    supporting evn variables

--- api/config.php
+++ api/config.php
@@ -3,10 +3,21 @@
 class Config{
     const DATE_FORMAT = "Y-m-d H:i:s";
 
-    const DB_HOST = "localhost";
-    const DC_USERNAME = "root";
-    const DB_PASSWORD = "KagamiTaiga10";
-    const DB_SCHEME = "webbenjamin";
+    public static function DB_HOST(){
+        return Config::get_env("DB_HOST", "localhost");
+      }
+      public static function DB_USERNAME(){
+        return Config::get_env("DB_USERNAME", "root");
+      }
+      public static function DB_PASSWORD(){
+        return Config::get_env("DB_PASSWORD", "KagamiTaiga10");
+      }
+      public static function DB_SCHEME(){
+        return Config::get_env("DB_SCHEME", "webbenjamin");
+      }
+      public static function DB_PORT(){
+        return Config::get_env("DB_PORT", "8082");
+      }
 
     const JWT_SECRET = "uhaiufhfbyuesfbiauk3fby3";
     const JWT_TOKEN_TIME = 604800;
