4ce9105b938968b7a87e21d8d06497c310ecf7cd
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-27 02:29:41 +0200

    added fucntion to search by article id

--- api/dao/TagsDao.class.php
+++ api/dao/TagsDao.class.php
@@ -7,6 +7,9 @@ class TagsDao extends BaseDao{
     public function __construct(){
         parent::__construct("tags");
     }
+    public function getTags($article_id){
+        return $this->query("SELECT * FROM tags WHERE article_id = :article_id LIMIT 10 OFFSET 0", ["article_id" => $article_id]);
+    }
 }
 
 ?>
