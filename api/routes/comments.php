b516c67af23114417f30abfa55900478f9194dcc
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-28 19:11:45 +0200

    documenting comments user routes

--- api/routes/comments.php
+++ api/routes/comments.php
@@ -10,7 +10,24 @@
         Flight::json($comments);
     });
 
-    Flight::route('POST /comments',function(){
+      /**
+ * @OA\Post(path="/user/comments", tags={"x-user", "comments"}, security={{"ApiKeyAuth": {}}},
+ *   @OA\RequestBody(description="Basic article info", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    				 @OA\Property(property="user_id", required="true", type="string", example="1",	description="user id" ),
+ *    				 @OA\Property(property="article_id", required="true", type="string", example="1",	description="article id" ),
+ *    				 @OA\Property(property="comment_text", type="string", example="Lorem",	description="comment" )
+ *                   @OA\Property(property="added_at", required="true", type="string", example="324234324",	description="time at which it was added" ),
+ *    				 @OA\Property(property="username", type="string", example="Benjamin",	description="username" )
+ *          )
+ *       )
+ *     ),
+ *  @OA\Response(response="200", description="Article added")
+ * )
+ */
+
+    Flight::route('POST /user/comments',function(){
         $request = Flight::request();
         $data = $request->data->getData();
         $category = Flight::commentsService()->add($data); 
