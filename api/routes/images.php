d4cd3f5fd88c701c2bd12c88524e2d3eaee48de4
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-28 19:11:55 +0200

    documenting images used routes

--- api/routes/images.php
+++ api/routes/images.php
@@ -6,10 +6,24 @@
  * )
  */
     Flight::route('GET /user/images/@id',function($id){
-        $images = Flight::imagesService()->get_article_images($id);  //images dao function for searching images by article ID is needed
+        $images = Flight::imagesService()->get_article_images($id);  
         Flight::json($images);
     });
 
+     /**
+ * @OA\Post(path="/admin/images", tags={"x-admin", "images"}, security={{"ApiKeyAuth": {}}},
+ *   @OA\RequestBody(description="Basic article info", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    				 @OA\Property(property="article_id", required="true", type="integer", example="1",	description="article id of the article we want to add a message to" ),
+ *    				 @OA\Property(property="picture64", required="true", type="string", example="subject",	description="article image" ),
+ *    				 
+ *          )
+ *       )
+ *     ),
+ *  @OA\Response(response="200", description="Image added to article")
+ * )
+ */
     Flight::route('POST /admin/images',function(){
         $request = Flight::request();
         $data = $request->data->getData();
